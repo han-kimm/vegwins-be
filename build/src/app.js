f09d614b12d3e3f022ea1cfbd4766204dde342a2
Haneul-Kim <4188989@naver.com>
2024-04-14 20:16:50 +0900

    setting: typescipt express 설치

---
+++ build/src/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.set("port", process.env.PORT || 8080);
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "번 포트에서 서버 시작,");
+});
