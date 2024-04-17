772bac603b47fa6a9bec400c8d326f345d931928
han-kimm <4188989@naver.com>
2024-04-17 18:14:34 +0900

    feat: "/api" 경로에서 cors 설정

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,6 @@
-import cors from "cors";
 import { Router } from "express";
 import { googleStrategy } from "../controllers/auth";
 const authRouter = Router();
-
-authRouter.use(
-  cors({
-    origin: ["http://localhost:3000", "https://vegwins.vercel.app"],
-    credentials: true,
-  })
-);
 
 authRouter.post("/google", googleStrategy);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import paperRouter from "./paper";
 import authRouter from "./auth";
+import cors from "cors";
 
 const indexRouter = Router();
+
+indexRouter.use(
+  cors({
+    origin: ["http://localhost:3000", "https://vegwins.vercel.app"],
+    credentials: true,
+  })
+);
 
 indexRouter.use("/paper", paperRouter);
 indexRouter.use("/auth", authRouter);
